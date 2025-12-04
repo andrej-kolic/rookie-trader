92ec6258a6d37e68e5c42efe91cbec9702123d7e
Rey <andrej.kolic@gmail.com>
2025-12-04 14:07:04 +0100

    cleanup

--- packages/app-core/src/index.tsx
+++ packages/app-core/src/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { add } from '@repo/ui/utils';
-import type { CustomType } from '@repo/commons';
 import { debugLog } from './utils/debug';
 import { TradingPairSelectorContainer } from './containers/TradingPairSelectorContainer';
 import { TickerDisplayContainer } from './containers/TickerDisplayContainer';
@@ -10,19 +8,7 @@ import { PriceChartContainer } from './containers/PriceChartContainer';
 import '@repo/ui/theme.css';
 import './styles.css';
 
-// TODO remove
-/* _eslint no-console: "error" */
-
-// TODO remove
-const ct: CustomType = { _type: 'test' };
-console.log('* custom type:', ct);
-
-// TODO remove
-console.log('* 1 + 2:', add(1, 2));
-
-const _orphan = 1;
-
-export function AppCore(props: {
+export function AppCore(_props: {
   className?: string;
   title: string;
   children: React.ReactNode;
@@ -43,8 +29,3 @@ export function AppCore(props: {
     </div>
   );
 }
-
-// TODO: remove - example of lint warning for eslint-plugin-react-refresh
-export const foo = () => {
-  //
-};
