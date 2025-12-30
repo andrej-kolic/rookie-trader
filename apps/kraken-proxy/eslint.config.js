5449d6c3d505878f0bc11940f24233b454d25420
Rey <andrej.kolic@gmail.com>
2025-12-30 15:07:18 +0100

    remove kraken-proxy from monorepo

--- apps/kraken-proxy/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { config as expressConfig } from '@repo/eslint-config/express';
-
-/** @type {import("eslint").Linter.Config} */
-export default [...expressConfig];
--- packages/eslint-config/express.js
+++
@@ -1,9 +0,0 @@
-import { config as baseConfig } from './base.js';
-
-export { disableTypeCheck } from './base.js';
-
-/**
- * A custom ESLint configuration for expressjs.
- *
- * @type {import("eslint").Linter.Config} */
-export const config = [...baseConfig];
